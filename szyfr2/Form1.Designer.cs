8ba0de597ca078a808eed90e553d56d008e79ccd
c1nek <gumball300@gmail.com>
2014-10-24 23:06:22 +0200

    konczymy

---
+++ szyfr2/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace szyfr2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tekstJawny = new System.Windows.Forms.TextBox();
+            this.tekstZaszyfrowany = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Utwórz alfabet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tekstJawny
+            // 
+            this.tekstJawny.Location = new System.Drawing.Point(114, 43);
+            this.tekstJawny.Multiline = true;
+            this.tekstJawny.Name = "tekstJawny";
+            this.tekstJawny.Size = new System.Drawing.Size(729, 292);
+            this.tekstJawny.TabIndex = 2;
+            // 
+            // tekstZaszyfrowany
+            // 
+            this.tekstZaszyfrowany.Location = new System.Drawing.Point(114, 341);
+            this.tekstZaszyfrowany.Multiline = true;
+            this.tekstZaszyfrowany.Name = "tekstZaszyfrowany";
+            this.tekstZaszyfrowany.Size = new System.Drawing.Size(729, 289);
+            this.tekstZaszyfrowany.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tekst jawny:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tekst zaszyfrowany:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(878, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Poziomo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(878, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Góra-dól";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(878, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Dół-góra";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Klucz:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(875, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Szyfruj";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(875, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Odszyfruj";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(878, 357);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Poziomo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(878, 386);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Góra-dół";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(878, 415);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Dół - góra";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 645);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tekstZaszyfrowany);
+            this.Controls.Add(this.tekstJawny);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tekstJawny;
+        private System.Windows.Forms.TextBox tekstZaszyfrowany;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ szyfr2/Form1.cs
@@ -0,0 +1,575 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szyfr2
+{
+    public partial class Form1 : Form
+    {
+        char[,] macierz = new char[5, 5];
+        char[] alfabet = new char[25];
+        string klucz, _alfabet;
+        int licznik = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("Start programu.");
+            domyslnyAlfabet();
+            //wypelnijMacierzDomyslnie();
+        }
+
+        string usunSpacje(string lancuch)
+        {
+            string bezSpacji;
+            bezSpacji = lancuch.Replace(" ", "");
+            return bezSpacji;
+        }
+
+        public static T[] MergeArrays<T>(T[] first, T[] second)
+        {
+            T[] result = new T[first.Length + second.Length];
+            Array.Copy(first, result, first.Length);
+            Array.Copy(second, 0, result, first.Length, second.Length);
+            return result;
+        }
+
+        void domyslnyAlfabet()
+        {
+            int k = 0;
+
+            for (int j = 0; j < 25; j++)
+            {
+                if (65 + k == 86) k++;
+                alfabet[j] = (char)(65 + k);
+                k++;
+            }
+
+        }
+
+        void alfabetZKluczem()
+        {
+            int i = 0;
+            char temp = klucz[0];
+            char[] nowyAlfabet = new char[50];
+            while (temp != 0)
+            {
+                nowyAlfabet[i] = klucz[i];
+                temp = klucz[i+1];
+                i++;
+            }
+            Array.Resize(ref nowyAlfabet, i);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Tworzenie alfabetu zawierającego klucz...");
+            
+            char[] alfabetTemp = MergeArrays(nowyAlfabet, alfabet);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            _alfabet = String.Join("", alfabetTemp);
+            Console.WriteLine("Alfabet uwzględniający klucz: " + _alfabet);
+
+            alfabet = usunDuplitakyZAlfabetu(alfabetTemp);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            _alfabet = String.Join("", alfabet);
+            Console.WriteLine("Alfabet z usunietymi duplikatami: " + _alfabet);
+            Console.ResetColor();
+
+       
+        }
+
+        void wypelnijMacierz(char[] _alfabet)
+        {
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    macierz[i,j]=_alfabet[count];
+                    count++;
+                }
+            }
+        }
+
+        void usunDuplikatyZKlucza(){
+
+            licznik = 0;
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Usuwanie duplikatow z klucza...");
+
+        char[] kluczTemp = new char[25];
+        
+            for (int j = 0; j < klucz.Length; j++)
+            {
+                if (PrzeszukajSlowo(kluczTemp, klucz[j]) == true)
+                {
+                    kluczTemp[licznik] = klucz[j];
+                    licznik++;
+                }
+                else
+                    continue;
+            }
+            
+        Console.ForegroundColor = ConsoleColor.Red;
+        klucz = String.Join("", kluczTemp);
+        Console.WriteLine("Klucz z usunietymi duplikatami: " + klucz);
+        Console.ResetColor();
+        }
+
+        char[] usunDuplitakyZAlfabetu(char[] alfabet)
+        {
+            licznik=0;
+            char[] nowyAlfabet = new char[25];
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Usuwanie duplikatow z alfabetu...");
+        
+            for (int j = 0; j < alfabet.Length; j++)
+            {
+                if (PrzeszukajSlowo(nowyAlfabet, alfabet[j]) == true)
+                {
+                    nowyAlfabet[licznik] = alfabet[j];
+                    licznik++;
+                }
+                else
+                    continue;
+            }
+            return nowyAlfabet;
+        }
+
+        bool PrzeszukajSlowo(char[] slowo, char litera)
+        {
+            for (int i = 0; i < licznik; i++)              
+            {
+                if(slowo[i] == litera)
+                {
+                    return false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return true;      
+        }
+
+        void wypiszMacierz()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Generowanie macierzy z alfabetem uwzgledzniajacym zadany klucz...");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Aktualnie używana macierz: ");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(macierz[i, j]+" ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ResetColor();
+        }
+
+        int numerKol(char literka)
+        {
+            int x=0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (literka == macierz[i, j])
+                    {
+                        x = j;
+                        return x+1;
+                    }
+                }
+            }
+
+            return (int)literka;   
+        }
+
+        int numerWer(char literka)
+        {
+            int x = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (literka == macierz[i, j])
+                    {
+                        x = i;
+                        return x + 1;
+                    }
+                }
+            }
+
+            return (int)literka;
+        }
+        char[,] tabPomoc(string tekstJawny)
+        {
+            char[,] tekstJawnyTab = new char[3, tekstJawny.Length];
+            
+
+            for (int i = 0; i < tekstJawny.Length; i++)
+            {
+                if (tekstJawny[i] == 32)
+                {
+                    tekstJawnyTab[0, i] = tekstJawny[i];
+                    tekstJawnyTab[1, i] = (char)0;
+                    tekstJawnyTab[2, i] = (char)0;
+                }
+                else
+                {
+                    tekstJawnyTab[0, i] = tekstJawny[i];
+                    tekstJawnyTab[1, i] = (char)numerKol(tekstJawnyTab[0, i]);
+                    tekstJawnyTab[2, i] = (char)numerWer(tekstJawnyTab[0, i]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Tworzenie tabeli pomocniczej...");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Tabela pomocnicza: ");
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < tekstJawny.Length; j++)
+                {
+                    if (i == 0 || tekstJawnyTab[i,j] == 48)
+                    {
+                        Console.Write((char)(tekstJawnyTab[i, j]));
+                    }
+                    else
+                    {
+                        Console.Write((int)(tekstJawnyTab[i, j]));
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+            return tekstJawnyTab;
+        }
+
+        char OdczytajZMacierzy(int wsp2, int wsp1)
+        {
+            char literka;
+           wsp1 -= 1;
+           wsp2 -= 1; 
+            literka = macierz[wsp1, wsp2];
+            Console.Write((wsp2+1) +","+ (wsp1+1) +" "+ literka +"\t");
+            return literka;
+        }
+        char OdczytajZMacierzy2(int wsp1, int wsp2)
+        {
+            char literka;
+           // wsp1 -= 1;
+           // wsp2 -= 1;
+            literka = macierz[wsp2, wsp1];
+            Console.Write((wsp1 + 1) + "," + (wsp2 + 1) + " " + literka + "\t");
+            return literka;
+        }
+      
+
+        //SZYFROWANIE
+
+        void szyfrPoz(char[,] tabPomoc)
+        {
+            string szyfr;
+            int licznik = 0;
+            int[] tabKod = new int[(tabPomoc.Length/3)*2];
+            int[,] tabSzyfr = new int[2, tabPomoc.Length];
+            char[] _szyfr = new char[tabPomoc.Length / 3];
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Szyfrowanie...");
+
+            for (int i = 0; i < (tabPomoc.Length/3); i++)
+            {
+                tabKod[i] = tabPomoc[1, i];
+                tabKod[(tabPomoc.Length/3) + i] = tabPomoc[2, i];
+            }
+
+            for (int i = 0; i < (tabPomoc.Length / 3); i++)
+            {
+                _szyfr[i] = OdczytajZMacierzy(tabKod[licznik], tabKod[licznik + 1]);
+                licznik += 2; ;
+            }
+
+            szyfr = string.Join("",_szyfr);
+
+            tekstZaszyfrowany.Text = szyfr;
+        }
+
+        void szyfrGD(char[,] tabPomoc)
+        {
+            string szyfr;
+            int licznik = 0;
+            int[] tabKod = new int[(tabPomoc.Length / 3) * 2];
+            int[,] tabSzyfr = new int[2, tabPomoc.Length];
+            char[] _szyfr = new char[tabPomoc.Length / 3];
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Szyfrowanie...");
+
+            for (int i = 0; i < (tabPomoc.Length / 3); i++)
+            {
+                if (i == (tabPomoc.Length / 3) - 1)
+                {
+                    tabKod[((tabPomoc.Length / 3) * 2) - 2] = tabPomoc[1, (tabPomoc.Length / 3) - 1];
+                    tabKod[((tabPomoc.Length / 3) * 2) - 1] = tabPomoc[2, 0];
+                }
+                else
+                {
+                    tabKod[licznik] = tabPomoc[1, i];
+                    tabKod[licznik + 1] = tabPomoc[2, i + 1];
+                    licznik += 2;
+                }
+            }
+            licznik = 0;
+
+            for (int i = 0; i < (tabPomoc.Length / 3); i++)
+            {
+                _szyfr[i] = OdczytajZMacierzy2(tabKod[licznik]-1, tabKod[licznik + 1]-1);
+                licznik += 2; ;
+            }
+
+            szyfr = string.Join("", _szyfr);
+
+            tekstZaszyfrowany.Text = szyfr;
+
+        }
+
+        void szyfrDG(char[,] tabPomoc)
+        {
+            string szyfr;
+            int licznik = 0;
+            int[] tabKod = new int[(tabPomoc.Length / 3) * 2];
+            int[,] tabSzyfr = new int[2, tabPomoc.Length];
+            char[] _szyfr = new char[tabPomoc.Length / 3];
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Szyfrowanie...");
+
+            for (int i = 0; i < (tabPomoc.Length / 3); i++)
+            {
+                if (i == (tabPomoc.Length / 3) - 1)
+                {
+                    tabKod[((tabPomoc.Length / 3) * 2) - 2] = tabPomoc[2, (tabPomoc.Length / 3) - 1];
+                    tabKod[((tabPomoc.Length / 3) * 2) - 1] = tabPomoc[1, 0];
+                }
+                else
+                {
+                    tabKod[licznik+1] = tabPomoc[1, i+1];
+                    tabKod[licznik] = tabPomoc[2, i];
+                    licznik += 2;
+                }
+            }
+            licznik = 0;
+
+            for (int i = 0; i < (tabPomoc.Length / 3); i++)
+            {
+                _szyfr[i] = OdczytajZMacierzy2(tabKod[licznik] - 1, tabKod[licznik + 1] - 1);
+                licznik += 2; ;
+            }
+
+            szyfr = string.Join("", _szyfr);
+
+            tekstZaszyfrowany.Text = szyfr;
+
+        }
+
+        //DESZYFROWANIE
+
+        string deSzyfrPoz(string szyfr)
+        {
+            int wsp1, wsp2;
+            int[] tabKod = new int[szyfr.Length * 2];
+            char[,] tabPomoc = new char[3, szyfr.Length];
+            char[] tekstJawny = new char[szyfr.Length];
+            int licznik = 0;
+            string rozszyfr;
+
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                wsp1 = numerKol(szyfr[i]);
+                wsp2 = numerWer(szyfr[i]);
+                tabKod[licznik] = wsp1;
+                tabKod[licznik+1] = wsp2;
+                licznik += 2;
+            }
+            licznik =0;
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                tabPomoc[1, i] = (char)tabKod[i];
+                tabPomoc[2, i] = (char)tabKod[tabKod.Length/2+i];
+                tabPomoc[0, i] = OdczytajZMacierzy2(tabKod[i]-1, tabKod[tabKod.Length / 2 + i]-1);
+                tekstJawny[i] = tabPomoc[0, i];
+                licznik += 2;
+
+            }
+
+            rozszyfr = string.Join("", tekstJawny);
+
+            return rozszyfr;
+
+        }
+
+        string deSzyfrGD(string szyfr)
+        {
+            int wsp1, wsp2;
+            int[] tabKod = new int[szyfr.Length * 2];
+            char[,] tabPomoc = new char[3, szyfr.Length];
+            char[] tekstJawny = new char[szyfr.Length];
+            int licznik = 0;
+            string rozszyfr;
+
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                wsp1 = numerKol(szyfr[i]);
+                wsp2 = numerWer(szyfr[i]);
+                tabKod[licznik] = wsp1;
+                tabKod[licznik + 1] = wsp2;
+                licznik += 2;
+            }
+            licznik = 0;
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                if (i == (tabKod.Length/2) - 1)
+                {
+                    tabPomoc[2, 0] = (char)tabKod[tabKod.Length-1];
+                    tabPomoc[1, (tabKod.Length / 2)-1] = (char)tabKod[tabKod.Length - 2];
+
+                }
+                else
+                {
+                    tabPomoc[1, i] = (char)tabKod[licznik];
+                    tabPomoc[2, i + 1] = (char)tabKod[licznik+1];
+                    licznik += 2;
+                 }
+
+            }
+
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                tabPomoc[0, i] = OdczytajZMacierzy2(tabPomoc[1,i]-1, tabPomoc[2,i]-1);
+                tekstJawny[i] = tabPomoc[0, i];
+            }
+            rozszyfr = string.Join("", tekstJawny);
+
+            return rozszyfr;
+
+        }
+
+        string deSzyfrDG(string szyfr)
+        {
+            int wsp1, wsp2;
+            int[] tabKod = new int[szyfr.Length * 2];
+            char[,] tabPomoc = new char[3, szyfr.Length];
+            char[] tekstJawny = new char[szyfr.Length];
+            int licznik = 0;
+            string rozszyfr;
+
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                wsp1 = numerKol(szyfr[i]);
+                wsp2 = numerWer(szyfr[i]);
+                tabKod[licznik] = wsp1;
+                tabKod[licznik + 1] = wsp2;
+                licznik += 2;
+            }
+            licznik = 0;
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                if (i == (tabKod.Length / 2) - 1)
+                {
+                    tabPomoc[1, 0] = (char)tabKod[tabKod.Length - 1];
+                    tabPomoc[2, (tabKod.Length / 2) - 1] = (char)tabKod[tabKod.Length - 2];
+
+                }
+                else
+                {
+                    tabPomoc[1, i+1] = (char)tabKod[licznik+1];
+                    tabPomoc[2, i] = (char)tabKod[licznik];
+                    licznik += 2;
+                }
+
+            }
+
+            for (int i = 0; i < szyfr.Length; i++)
+            {
+                tabPomoc[0, i] = OdczytajZMacierzy2(tabPomoc[1, i] - 1, tabPomoc[2, i] - 1);
+                tekstJawny[i] = tabPomoc[0, i];
+            }
+            rozszyfr = string.Join("", tekstJawny);
+
+            return rozszyfr;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Brak wskazanego klucza.");
+                Console.ResetColor();
+                wypelnijMacierz(alfabet);
+                wypiszMacierz();
+            }
+            else
+            {
+                klucz = textBox1.Text;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wskazany klucz: " + klucz);
+                Console.ResetColor();
+                usunDuplikatyZKlucza();
+                alfabetZKluczem();
+                wypelnijMacierz(alfabet);
+                wypiszMacierz();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {         
+            szyfrPoz(tabPomoc(usunSpacje(tekstJawny.Text)));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tekstJawny.Text = deSzyfrPoz(usunSpacje(tekstZaszyfrowany.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            szyfrGD(tabPomoc(usunSpacje(tekstJawny.Text)));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tekstJawny.Text = deSzyfrGD(usunSpacje(tekstZaszyfrowany.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            szyfrDG(tabPomoc(usunSpacje(tekstJawny.Text)));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tekstJawny.Text = deSzyfrDG(usunSpacje(tekstZaszyfrowany.Text));
+        }
+    }
+}
